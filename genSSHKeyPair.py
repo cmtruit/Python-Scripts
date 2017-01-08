77df91ed3760c55c7b96a85561b9e2655281726c
Chris Truitt <christruitt@msn.com>
2017-01-08 10:44:50 -0500

    commit script to create ssh key pair

---
+++ genSSHKeyPair.py
@@ -0,0 +1,44 @@
+import os
+from os import chmod
+from Crypto.PublicKey import RSA
+
+class GenerateSSHKey():
+    def __init__(self, user):
+        self.user = user
+        self.dir = '/tmp/sshkeys/'
+        self.location = self.dir + self.user
+        self.key = RSA.generate(2048)
+        self.ext = (".key")
+ 
+    def PrivateKey(self):
+        self.t = "_private"
+        self.file = self.location + self.t + self.ext
+        if not os.path.exists(self.location):
+            try:
+                os.makedirs(self.location)
+            except:
+                raise OSError
+        with open(self.file, 'w') as content_file:
+          chmod(self.file, 0600)
+          content_file.write(self.key.exportKey('PEM'))
+        return self.file
+ 
+    def PublicKey(self):
+        self.pubkey = self.key.publickey()
+        self.t = "_public"
+        self.file = self.location + self.t + self.ext
+        if not os.path.exists(self.location):
+            try:
+                os.makedirs(self.location)
+            except:
+                raise OSError
+        with open(self.file, 'w') as content_file:
+          content_file.write(self.pubkey.exportKey('OpenSSH'))
+        return self.file
+
+user = "myusername"
+
+Gen = GenerateSSHKey(user)
+
+print Gen.PrivateKey() 
+print Gen.PublicKey() 
